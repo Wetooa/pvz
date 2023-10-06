e732304caf6edf7c696e3261b32862f085d57f9a
LAB12_USER ON WS29 <L12X17W29@citulab.net>
2023-10-06 16:16:50 +0800

    asshole

--- Main.java
+++ Main.java
@@ -1,4 +1,50 @@
-class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-  public static void main(String[] args) {}
+public class Main {
+    public static void main(String[] args) {
+        List<Plant> plants = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Game Mode: ");
+        String mode = sc.nextLine();
+
+        String input;
+        do {
+            System.out.print("Add a plant: ");
+            input = sc.nextLine();
+            switch (input) {
+                case "DONE":
+                    break;
+                case "Wall Nut":
+                    plants.add(new Plant.WallNut());
+                    break;
+                case "Sun-shroom":
+                    plants.add(new Mushroom.SunShroom(false));
+                    break;
+                // add more plants here
+                default:
+                    System.out.println(input + " is not a plant");
+            }
+        } while (!input.equals("DONE"));
+
+        do {
+            System.out.print("Do something: ");
+            input = sc.nextLine();
+            switch (input) {
+                case "DONE":
+                    break;
+                case "Produce Sun":
+                    // add implementation here
+                    break;
+                case "Attack":
+                    // add implementation here
+                    break;
+                // add more cases here
+                default:
+                    System.out.println("Unknown action: " + input);
+            }
+        } while (!input.equals("DONE"));
+    }
 }
